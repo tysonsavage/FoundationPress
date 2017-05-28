432a9bd72460c96452fb273c0b1663d337a2c85b
Mark Savage <tysonsavage@gmail.com>
2017-05-28 21:54:07 +0100

    ms

--- assets/javascript/custom/site.js
+++ assets/javascript/custom/site.js
@@ -2,7 +2,7 @@
 $(function() {
 
     $( ".entry-content p" ).each(function() {
-        var watcher = scrollMonitor.create( $(this), -65 );
+        var watcher = scrollMonitor.create( $(this), -50 );
         watcher.enterViewport(function() {
             $(this.watchItem).addClass('fade-in');
             this.destroy();
